1633f216b16302c3e64190cad718fdf0ab406745
martinmajor88 <mmajor@intrepidinternational.com>
2015-03-26 19:42:32 -0400

    Update piratebar.py

--- piratebar.py
+++ piratebar.py
@@ -17,39 +17,44 @@ ingredients = {
 }
 
 
+print "Welcome to the Sandy Shorts Pirate Bar! Tell me how ya like yer poison!"
+print ""
+
 def preference ():
-  prefs = []
+  global prefs
+  prefs = {}
   for key, question in questions.items ():
-    print key, "->", question
     my_preference = raw_input ("{} (y/n): ".format (question))
     if my_preference == "y":
-      print "I want a drink thats ", key
-      prefs.append (key)
+      print "So ya like'em", key, "? Great! a pirate after me own heart!"
+      prefs[key]=True
     elif my_preference == "n":
-      print "I don't want to drink that ", key
-    else :
+      print "So ya don't like'em", key, "? No problem matey!"
+      prefs[key]=False
+    else:
       print "WTF, Yes or No ?!?!?"
   return prefs
-  
-def drink (prefs):
-  a = hash(prefs)
-  b = ingredients(a)
+
+def drink(preference):
   hooch = []
-  for pair in b:
-    if prefs == pair[0]:
-      print "So ya like em", prefs, "? that'll be ", ingredient
-      hooch.append (ingredient)
-    else:
-      continue
+  for flavor, choice in prefs.items():
+      if choice == True:
+        hooch.append (random.choice(ingredients[flavor]))
+      else:
+        continue
   return hooch
 
-preference ()
-print drink(preference)
+def main():
+  preference()
+  hooch = drink(preference)
+  print ""
+  print "Alrighty matey, that'll be:"
+  for ingredient in hooch:
+    print "A {}" .format(ingredient)
+  print ""
+  print "Enjoy yer poison ya scallywag!"
+  
+if __name__ == "__main__":
+  main()
 
-
-
-
-
-
-
-    
+#Main question as of this iteration of the program is how to get the ingredients to print out in a string form (e.g ingredient 1, ingredient 2, etc...) instead of on seperate lines.      
